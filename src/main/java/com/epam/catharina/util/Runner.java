9ad4f754e240e81834664b2f4260f7918d96db9d
Alexey Slepenkov <alexey.slepenkov@gmail.com>
2014-08-12 21:58:55 +0600

    email sending block+object creating

--- src/main/java/com/epam/catharina/util/Runner.java
+++ src/main/java/com/epam/catharina/util/Runner.java
@@ -11,4 +11,18 @@ public class Runner {
         User su = new User(UUID.randomUUID(), "General_Elvis", "alexey.slepenkov@gmail.com", MyRole.ADMIN , Sex.MALE, Date.valueOf("1994-03-26"));
         System.out.println("su = " + su.toString());
     }
+
 }
+
+
+
+//TODO QUESTIONS!!
+/*
+1-Сразу в sql или агрегирующий класс?
+2-КОпия состояния в объектах, или состояние в базе
+3-Хранение картинок  - база, объект, реляц база?
+4-
+5-
+6-
+7-
+ */
